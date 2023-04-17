d461fa8a6ba4cf5b3f980716bea4bcf3fafa5f9f
Yurii Rybachok <drummer2009@ukr.net>
2023-04-17 20:14:18 +0300

    user page contentOverlay component

--- resources/js/Pages/User.vue
+++ resources/js/Pages/User.vue
@@ -2,6 +2,7 @@
 import { reactive, toRefs } from 'vue';
 import { Head, Link, router } from '@inertiajs/vue3';
 import MainLayout from '@/Layouts/MainLayout.vue';
+import ContentOverlay from '@/Components/ContentOverlay.vue';
 
 import Cog from 'vue-material-design-icons/Cog.vue';
 import Grid from 'vue-material-design-icons/Grid.vue';
